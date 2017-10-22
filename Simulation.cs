82ec90216a5093883b8e6fc87860bf41cfb18bc6
KrecksFre <32411809+KrecksFre@users.noreply.github.com>
2017-10-22 18:42:05 +0200

    Add files via upload
    EinfacheAusgabe ist die graphische ausgabe bei 400 durchläufen
    Die anderen drei sind die überarbeiteten Codes

---
+++ Simulation.cs
@@ -0,0 +1,92 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: freudis
+ * Date: 06.10.2017
+ * Time: 18:24
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.IO;
+
+namespace NagelSchreckenberg
+{
+	/// <summary>
+	/// Description of Simulation.
+	/// </summary>
+	public class Simulation
+	{
+		public int schritte = 0;
+		public Auto[] autos = new Auto[40];
+		
+		public Simulation()
+		{
+			for (int i = 0; i < autos.Length; i++) {
+				autos[i] = new Auto();
+			}
+			int Position = 2;
+			foreach (Auto a in autos)
+			{
+				a.Position = Position;
+				Position = Position + 4;
+			}
+		}
+		public void NaSch() {
+			for (int i = 0; i < autos.Length; i++) {
+				Auto vordermann = autos[(i+1) % autos.Length];
+//				Console.Write("Auto: " + i + " -> v: " + autos[i].Geschwindigkeit );
+				autos[i].Beschleunigen();
+//				Console.Write(" -> a: " + autos[i].Geschwindigkeit );
+				autos[i].Bremsen(vordermann);
+//				Console.Write(" -> b: " + autos[i].Geschwindigkeit );
+				autos[i].Trödeln();
+//				Console.WriteLine(" -> t: " + autos[i].Geschwindigkeit );
+			}
+			foreach (Auto a in autos)
+			{
+				a.Fahren();
+			}
+			schritte++;
+		}
+		
+		public void EinfacheAusgabe(string pfadName) {
+			
+			using ( StreamWriter einfacheAusgabe = File.AppendText(pfadName))
+			{
+				System.Text.StringBuilder einfacheAusgabeText = new System.Text.StringBuilder();
+				
+				for (int Länge = 0; Länge < Auto.Straße; Länge++)
+				{
+					einfacheAusgabeText.Append("").Append(".").Append("");
+				}
+				
+				foreach (Auto a in autos)
+				{
+//				Console.WriteLine("debug: " + a.Position + a.Geschwindigkeit);
+					einfacheAusgabeText[Convert.ToInt32(a.Position)] = Convert.ToChar(Convert.ToString(Convert.ToInt32(a.Geschwindigkeit)));
+				}
+				
+				einfacheAusgabe.WriteLine(einfacheAusgabeText.ToString());
+				//Console.WriteLine();
+				//Console.Write("Press any key to continue . . . ");
+				//Console.Clear;
+				//Console.ReadKey(true);
+			}
+		}
+		
+		public void Ausgeben(string pfadName) {
+			using ( StreamWriter ausgabe = File.AppendText(pfadName))
+			{
+				
+				//ausgabe.WriteLine("Schritt {0} der Simulation: ", schritte);
+				
+				foreach (Auto a in autos)
+				{
+					ausgabe.WriteLine("{0},{1}",schritte, a);
+				}
+				//ausgabe.WriteLine("--------------------------------");
+				
+			}
+		}
+	}
+}
