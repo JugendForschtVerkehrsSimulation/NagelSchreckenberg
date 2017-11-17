7f9c92618d71dc5e1554d0a7801d8c813ef355ca
Gregor Freudenberger <freudekrecks@gmail.com>
2017-11-17 18:56:32 +0100

    Klasse Spur erstellt

---
+++ NaSchAmpel/Spur.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: freudis
+ * Date: 17.11.2017
+ * Time: 18:54
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace NaSchAmpel
+{
+	/// <summary>
+	/// Description of Spur.
+	/// </summary>
+	public class Spur
+	{
+		public float Länge;
+		public List<Auto> autos = new List<Auto>();
+		public Spur()
+		{
+		}
+	}
+}
