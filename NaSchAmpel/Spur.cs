637331ebb14d87be4fe245f698e15e7b51178bc0
Gregor Freudenberger <freudekrecks@gmail.com>
2017-11-17 19:10:01 +0100

    Ampel Geschwindigkeit
    längere Beschreibung der Änderung

--- NaSchAmpel/Spur.cs
+++ NaSchAmpel/Spur.cs
@@ -15,10 +15,19 @@ namespace NaSchAmpel
 	/// </summary>
 	public class Spur
 	{
-		public float Länge;
+		public double Länge;
 		public List<Auto> autos = new List<Auto>();
 		public Spur()
 		{
+		}
+		
+		public double MaximaleGeschwindigkeit(float Position) {
+			if(Position < -30.0 || Position > 5) {
+				return 50/3.6;
+			}
+			double entfernung = Math.Abs(Position);
+			// TODO: maximalgeschwindigkeit bis zur Ampel linear auf 30 km/h reduzieren
+			return 30/3.6;
 		}
 	}
 }
